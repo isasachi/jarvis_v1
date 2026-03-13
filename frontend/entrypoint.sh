221f966c7d33ab0de77f5334cbe1f6cf40cafd3b
Isaac Salirrosas <isaacsalirrosasc@gmail.com>
2026-03-13 14:51:12 -0500

    third commit

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Reemplazar WebSocket URL
 sed -i "s|__BACKEND_WS_URL__|${BACKEND_WS_URL:-ws://localhost:8000/ws}|g" /usr/share/nginx/html/index.html
+
+# Reemplazar puerto dinámico de Railway
+export PORT=${PORT:-80}
+sed -i "s/\$PORT/$PORT/g" /etc/nginx/conf.d/default.conf
 
 exec "$@"
